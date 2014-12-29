b0673949d8c3812685a07423543a7f12dc2c7f2f
Matthew Smart <matthew@treasurylane.com>
2014-12-29 09:13:25 -0600

    initial commit

---
+++ MathQuiz/Form1.cs
@@ -0,0 +1,159 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MathQuiz
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        Random randomizer = new Random();
+        int addend1, addend2, minuend, subtrahend, factor1, factor2, dividend, divisor; 
+        int timeLeft;
+
+
+        public void StartTheQuiz()
+        {
+            // set up the addition problem
+            addend1 = randomizer.Next(51);
+            addend2 = randomizer.Next(51);
+            plusLeftLabel.Text = addend1.ToString();
+            plusRightLabel.Text = addend2.ToString();
+            sum.Value = 0;
+
+            // set up the subtraction problem
+            minuend = randomizer.Next(1, 101);
+            subtrahend = randomizer.Next(1, minuend);
+            minusLeftLabel.Text = minuend.ToString();
+            minusRightLabel.Text = subtrahend.ToString();
+            difference.Value = 0;
+
+            // set up the multiplication problem
+            factor1 = randomizer.Next(2, 11);
+            factor2 = randomizer.Next(2, 11);
+            timesLeftLabel.Text = factor1.ToString();
+            timesRightLabel.Text = factor2.ToString();
+            product.Value = 0;
+
+            // set up the division problem
+            divisor = randomizer.Next(2, 11); // second number
+            int temp = randomizer.Next(2, 11);
+            dividend = temp * divisor; // make sure the answer is an integer
+            divideLeftLabel.Text = dividend.ToString();
+            divideRightLabel.Text = divisor.ToString();
+            quotient.Value = 0;
+
+
+            timeLeft = 30;
+            timeLabel.BackColor = Control.DefaultBackColor;
+            timeLabel.Text = "30 seconds";
+            timer1.Start();
+
+        }
+        
+        
+        private void startButton_Click(object sender, EventArgs e)
+        {
+            StartTheQuiz();
+            startButton.Enabled = false;
+        }
+
+        /* when the timer expires, check the current answers
+         */
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            if (CheckTheAnswer()) // all answers are correct
+            {
+                timer1.Stop();
+                MessageBox.Show("You got all the answers correctly!", "Congratulations");
+                startButton.Enabled = true;
+            }
+            
+            else if (timeLeft > 0)
+            {
+                timeLeft--; 
+                if (timeLeft <= 5)
+                {
+                    timeLabel.BackColor = Color.Red;
+                }
+                
+                timeLabel.Text = timeLeft + " seconds";
+
+            }
+
+            else // time has expired without correct answwers
+            {
+                timer1.Stop();
+                timeLabel.Text = "Time's up!";
+                MessageBox.Show("You didn't finish in time.", "Sorry");
+                sum.Value = addend1 + addend2;
+                difference.Value = minuend - subtrahend;
+                product.Value = factor1 * factor2;
+                quotient.Value = dividend / divisor;
+                startButton.Enabled = true;
+            }
+        }
+
+        private bool CheckTheAnswer()
+        {
+            if ((addend1 + addend2 == sum.Value)
+                && (minuend - subtrahend == difference.Value)
+                && (factor1 * factor2 == product.Value)
+                && (dividend / divisor == quotient.Value))
+                return true;
+            else
+                return false;
+        }
+
+        // highlight (select) the text inside the answer box
+        private void answer_Enter(object sender, EventArgs e)
+        {
+            NumericUpDown answerBox = sender as NumericUpDown;
+
+            if (answerBox != null)
+            {
+                int lengthOfAnswer = answerBox.Value.ToString().Length;
+                answerBox.Select(0, lengthOfAnswer);
+            }
+        }
+
+        private void numericUpDown1_ValueChanged(object sender, EventArgs e)
+        {
+            sum.BackColor = Control.DefaultBackColor;
+            if (sum.Value == addend1 + addend2)
+                sum.BackColor = Color.Green;
+        }
+
+        private void difference_ValueChanged(object sender, EventArgs e)
+        {
+            difference.BackColor = Control.DefaultBackColor;
+            if (difference.Value == minuend - subtrahend)
+                difference.BackColor = Color.Green;
+        }
+
+        private void product_ValueChanged(object sender, EventArgs e)
+        {
+            product.BackColor = Control.DefaultBackColor;
+            if (product.Value == factor1 * factor2)
+                product.BackColor = Color.Green;
+        }
+
+        private void quotient_ValueChanged(object sender, EventArgs e)
+        {
+            quotient.BackColor = Control.DefaultBackColor;
+            if (quotient.Value == dividend / divisor)
+                quotient.BackColor = Color.Green;
+        }
+    }
+}
